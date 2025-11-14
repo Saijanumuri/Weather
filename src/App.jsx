66ad9880e008316199d293e8d8a5872528bf957f
Saijanumuri <srkbvm2002@gmail.com>
2025-11-14 15:06:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Weather from "./Weather"
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Weather/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Weather.jsx
@@ -0,0 +1,8 @@
+function Weather() {
+  return (
+    <div>
+      <h1>Weather Component</h1>
+    </div>
+  );
+}
+export default Weather;
